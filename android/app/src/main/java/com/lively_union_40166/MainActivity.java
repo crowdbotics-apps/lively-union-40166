cb55867747d98e121765212a65fd170b7f78e710
Crowdbotics <team@crowdbotics.com>
2023-04-24 13:58:20 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lively_union_40166/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lively_union_40166;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lively_union_40166";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-union-40166",
+  url: "https://lively-union-40166.botics.co",
+  api: "https://lively-union-40166.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
